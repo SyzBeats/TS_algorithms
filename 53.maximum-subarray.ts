d6a8f4a68f20b6ff284a4db53e504cf70ec34c7b
Simeon Zimmermann <syzdevacc@gmail.com>
2022-04-30 14:58:09 +0200

    feat: max subarray o(n)

--- 53.maximum-subarray.ts
+++ 53.maximum-subarray.ts
@@ -6,7 +6,17 @@
 
 // @lc code=start
 function maxSubArray(nums: number[]): number {
+	let sum = 0;
+	let max = nums[0];
 
-};
+	for (let i = 0; i < nums.length; i++) {
+		sum = sum + nums[i];
+		max = Math.max(max, sum);
+		if (sum < 0) {
+			sum = 0;
+		}
+	}
+
+	return max;
+}
 // @lc code=end
-
